664095d742504929808e2bf89cb25f2b4cdc7306
Mohamadkh01 <mohamadkh6324@gmail.com>
2025-05-13 15:52:32 +0300

    minor design changes

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ for(let i = 0; i < 16; i++){
 
     const row = document.createElement("div");
     row.style.display = "flex";
+    row.style.flex = "1"
 
     for(let j = 0; j < 16; j++){
         const div = document.createElement("div");
-        div.style.width = "40px";
         div.style.aspectRatio = "1 / 1";
         div.style.backgroundColor = "black";
         div.style.margin = "3px";
+        div.style.flex = "1";
         
         let flag = false;
 
