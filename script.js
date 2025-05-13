8119497d7f856cf64de0ffd67ad3bfac6749bf92
Mohamadkh01 <mohamadkh6324@gmail.com>
2025-05-13 22:26:41 +0300

    add a button to change the size of the grid depending on user input and a reset button to reset the sketch

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
-const grid = document.querySelector("div");
+const tops = document.querySelector(".top");
+const grid = document.querySelector(".grid");
 
-for(let i = 0; i < 16; i++){
+const sizeBtn = document.createElement("button");
+const resetBtn = document.createElement("button");
 
-    const row = document.createElement("div");
-    row.style.display = "flex";
-    row.style.flex = "1"
+let size = 16;
+let flag = false;
 
-    for(let j = 0; j < 16; j++){
-        const div = document.createElement("div");
-        div.style.aspectRatio = "1 / 1";
-        div.style.backgroundColor = "black";
-        div.style.margin = "3px";
-        div.style.flex = "1";
-        
-        div.addEventListener("mouseenter", ()=> div.style.backgroundColor = "aqua");
-        div.addEventListener("mouseout", ()=> div.style.backgroundColor = "black");
+grid.addEventListener("click", ()=> {if(flag == true){flag = false;} else {flag = true;}});
 
-        row.appendChild(div);
+sizeBtn.textContent = "grid size";
+sizeBtn.style.fontSize = "25px";
+sizeBtn.style.margin = "3px";
+sizeBtn.addEventListener("click", ()=> {do{size = prompt("enter grid size: ");} while(size < 2 || size > 50) loadGrid(size); flag = false;});
+
+resetBtn.textContent = "reset";
+resetBtn.style.fontSize = "25px";
+resetBtn.style.margin = "3px";
+resetBtn.addEventListener("click", ()=> {loadGrid(size); flag = false;});
+
+tops.appendChild(sizeBtn);
+tops.appendChild(resetBtn);
+
+function loadGrid(size){
+
+    grid.innerHTML = "";
+
+    for(let i = 0; i < size; i++){
+
+        const row = document.createElement("div");
+        row.style.display = "flex";
+        row.style.flex = "1"
+
+        for(let j = 0; j < size; j++){
+            const div = document.createElement("div");
+            div.style.aspectRatio = "1 / 1";
+            div.style.backgroundColor = "black";
+            div.style.margin = "3px";
+            div.style.flex = "1";
+
+            div.addEventListener("mouseenter", ()=> {if(flag == true){div.style.backgroundColor = "aqua"}});
+
+            row.appendChild(div);
+        }
+
+        grid.appendChild(row);
     }
-
-    grid.appendChild(row);
 }
+
+loadGrid(size);
